4ad0f8c7a9a829346d91383a8dbddc03b866329e
user <khodirjob@gmail.com>
2026-03-12 11:55:56 +0500

    loader

--- src/main/java/uz/java/yoshlar_tashabusi_app/loader/AgeCategoryLoader.java
+++ src/main/java/uz/java/yoshlar_tashabusi_app/loader/AgeCategoryLoader.java
@@ -23,7 +23,7 @@ public class AgeCategoryLoader implements CommandLineRunner {
 
 
         repository.saveAll(ageCategoryList());
-
+//        addressRepository.saveAll(addressList());
 
         System.out.println(">>> Yosh toifalari bazaga yuklandi!");
     }
@@ -54,7 +54,75 @@ public class AgeCategoryLoader implements CommandLineRunner {
 
     public List<Address> addressList() {
         List<Address> addresses = List.of(
-
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 13998, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 13999, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14000, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14001, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14002, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14003, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14004, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14005, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14006, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14007, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14008, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14009, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14010, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14011, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14012, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14013, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14014, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14015, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14016, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14017, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14018, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14019, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14020, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14021, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14022, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14023, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14024, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14025, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14026, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14027, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14028, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14029, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14030, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14031, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14032, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14033, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14034, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14035, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14036, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14037, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14038, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14039, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14040, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14041, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14042, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14043, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14044, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14045, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14046, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14047, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14048, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14049, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14050, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14051, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14052, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14053, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14054, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14055, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14056, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14057, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14058, "3-sektor (IIB sektori)", 6),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14059, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14060, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14061, "2-sektor (Prokuror sektori)", 5),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14062, "1-sektor (Xokim sektori)", 4),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14063, "4-sektor (Soliq sektori)", 7),
+                new Address(null, 3, "ANDIJON", "Paxtaobod", 45, 14064, "1-sektor (Xokim sektori)", 4)
         );
+
+        return addresses;
     }
 }
